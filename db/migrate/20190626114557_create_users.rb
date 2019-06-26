0a2244d8a75d6e7e08063da00a49c942497ba98e
Yuri V <yuri.valigursky@gmail.com>
2019-06-26 15:58:09 +0300

    user model

--- db/migrate/20190626114557_create_users.rb
+++ db/migrate/20190626114557_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
+    create_table :users, id: false do |t|
+      t.string :id, limit: 36, primary_key: true, null: false 
       t.string :user_name
       t.string :password_digest
 
